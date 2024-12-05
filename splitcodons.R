80fcce195083ee0c479aeadda6804240b8806fe4
felipelin <73185638+felipelin@users.noreply.github.com>
2024-12-05 17:27:13 +0800

    Update splitcodons.R

--- splitcodons.R
+++ splitcodons.R
@@ -3,11 +3,12 @@ library(ape)
 library(phylotools)
 
 raw = read.FASTA("./input/sequence.fas")
+n = 270 # individuals
 
 for(j in 1:3){
   extract_dna = NULL
   extract_dna[1]=j
-  for(i in 1:270){
+  for(i in 1:n){
     extract_dna[i+1]=j+3*i
   }
   write.FASTA(x = raw[extract_dna],
