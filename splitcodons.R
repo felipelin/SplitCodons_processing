5ee08bd1890f49124a42702d3038d428cc1345a1
felipelin <73185638+felipelin@users.noreply.github.com>
2024-12-05 17:25:15 +0800

    Add files via upload

---
+++ splitcodons.R
@@ -0,0 +1,15 @@
+# SplitCodons
+library(ape)
+library(phylotools)
+
+raw = read.FASTA("./input/sequence.fas")
+
+for(j in 1:3){
+  extract_dna = NULL
+  extract_dna[1]=j
+  for(i in 1:270){
+    extract_dna[i+1]=j+3*i
+  }
+  write.FASTA(x = raw[extract_dna],
+              file = paste("./output/Codon_",j,".fas",sep = ""))
+}
